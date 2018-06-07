63ba7877260782b762a749977f9db9ff4178d9c4
n0va- <nataliedefilippis@gmail.com>
2018-06-07 15:15:31 -0400

    Delete brigade.js

--- azure-vote/brigade.js
+++
@@ -1,8 +0,0 @@
-function kubeJobRunner (config, k) {
-    k.storage.enabled = false
-    k.image = "lachlanevenson/k8s-kubectl:v1.8.2"
-    k.tasks = [
-        `kubectl set image deployment/vote-deploy vote-web-cntnr=nataliedocker.azurecr.io/azureworkshop/azure-vote-front:${config.get("imageTag")}`
-    ]
-}
-
